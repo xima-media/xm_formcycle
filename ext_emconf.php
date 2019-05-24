7193ce4836739c584e1c426a71213953f3516dc5
Konrad Michalik <konrad.michalik@xima.de>
2019-05-24 14:11:52 +0200

    [Task] Abwärtskompatibilität

--- ext_emconf.php
+++ ext_emconf.php
@@ -28,7 +28,7 @@ $EM_CONF[$_EXTKEY] = array(
                 array(
                     'extbase' => '1.3-',
                     'fluid'   => '1.3-',
-                    'typo3'   => '6.2.0-9.5.99',
+                    'typo3'   => '7.6.0-9.5.99',
                 ),
             'conflicts' =>
                 array(),
