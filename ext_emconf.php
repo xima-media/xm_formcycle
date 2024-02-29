502be3a69d98e5fb48adcb3109a4d0e7756ec1a4
Maik Schneider <mas@xima.de>
2024-02-29 12:17:11 +0100

    feat: cleanup

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,32 +1,19 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext "xm_formcycle".
- *
- * Auto generated 03-12-2015 14:46
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
-/** @var TYPE_NAME $_EXTKEY */
 $EM_CONF[$_EXTKEY] = [
-    'title'            => 'FORMCYCLE',
-    'description'      => 'form management with professional form designer, process management, inbox and more features.',
-    'category'         => 'plugin',
-    'version'          => '8.0.1',
-    'state'            => 'stable',
-    'author'           => 'XIMA MEDIA GmbH',
-    'author_email'     => 'support@formcycle.de',
-    'author_company'   => 'XIMA MEDIA GmbH',
-    'constraints'      => [
-        'depends'   => [
-            'extbase' => '1.3-',
-            'fluid'   => '1.3-',
-            'typo3'   => '9.5.0-11.5.99',
+    'title' => 'formcylce',
+    'description' => 'Form management with professional form designer, process management, inbox and more.',
+    'category' => 'plugin',
+    'version' => '9.0.0',
+    'state' => 'stable',
+    'author' => 'Maik Schneider',
+    'author_email' => 'maik.schneider@xima.de',
+    'author_company' => 'XIMA Media GmbH',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.0.0-12.5.99',
         ],
         'conflicts' => [],
-        'suggets'   => [],
+        'suggets' => [],
     ],
 ];
--- ext_localconf.php
+++ ext_localconf.php
@@ -4,48 +4,6 @@ if (!defined('TYPO3')) {
     die('Access denied.');
 }
 
-$extConf = $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['xm_formcycle'];
-
-if (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(\TYPO3\CMS\Core\Utility\VersionNumberUtility::getNumericTypo3Version()) > 10000000) {
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-        'XmFormcycle',
-        'Xmformcycle',
-        [
-            \Xima\XmFormcycle\Controller\FormcycleController::class => 'list, formContent',
-        ],
-        // non-cacheable actions
-        $extConf['integrationMode'] == 'integrated' ? [\Xima\XmFormcycle\Controller\FormcycleController::class => 'list'] : []
-    );
-} else {
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-        'XmFormcycle',
-        'Xmformcycle',
-        [
-            \Xima\XmFormcycle\Controller\FormcycleController::class => 'list, formContent',
-        ],
-        // non-cacheable actions
-        $extConf['integrationMode'] == 'integrated' ? ['Formcycle' => 'list'] : []
-    );
-}
-
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1593170596] = [
-    'nodeName' => 'startNewElement',
-    'priority' => 40,
-    'class'    => \Xima\XmFormcycle\Form\Element\StartNewElement::class,
-];
-
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1593173971] = [
-    'nodeName' => 'startOpenElement',
-    'priority' => 40,
-    'class'    => \Xima\XmFormcycle\Form\Element\StartOpenElement::class,
-];
-
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1593174782] = [
-    'nodeName' => 'fcElement',
-    'priority' => 40,
-    'class'    => \Xima\XmFormcycle\Form\Element\FcElement::class,
-];
-
 // register custom form element
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1709039883] = [
     'nodeName' => 'formcycle-selection',
