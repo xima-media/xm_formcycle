c7e56c8ee559060660e47fb1683c29f55617554e
Maik Schneider <mas@xima.de>
2024-12-16 13:01:13 +0100

    feat: add site config and setting as fixture, simplify acceptance test

--- Tests/Acceptance/Backend/ElementCest.php
+++ Tests/Acceptance/Backend/ElementCest.php
@@ -2,11 +2,9 @@
 
 namespace Xima\XmFormcycle\Tests\Acceptance\Backend;
 
-use Codeception\Attribute\Depends;
 use Codeception\Scenario;
 use Xima\XmFormcycle\Tests\Acceptance\Support\AcceptanceTester;
 use Xima\XmFormcycle\Tests\Acceptance\Support\Enums\Selectors;
-use Xima\XmFormcycle\Tests\Acceptance\Support\Helper\ExtensionConfiguration;
 use Xima\XmFormcycle\Tests\Acceptance\Support\Helper\PageTree;
 use Xima\XmFormcycle\Tests\Acceptance\Support\Helper\ShadowDom;
 
@@ -22,45 +20,6 @@ class ElementCest
         $I->click('button[type="submit"]');
         $I->waitForElementNotVisible('form[name="loginform"]');
         $I->seeCookie('be_typo_user');
-    }
-
-    #[Depends('createElementAndSave')]
-    public function seeExtensionConfigurationError(
-        AcceptanceTester $I,
-        PageTree $pageTree
-    ): void {
-        $this->navigateToElementTab($I, $pageTree);
-        $I->see('Configuration error');
-        $I->seeElement('.callout-danger');
-    }
-
-    private function navigateToElementTab(
-        AcceptanceTester $I,
-        PageTree $pageTree,
-    ): void {
-        $I->click('Page');
-        $I->waitForElementVisible(PageTree::$treeSelector);
-        $I->wait(2);
-        $pageTree->openPath(['Main', 'Example']);
-        $I->switchToContentFrame();
-        $I->waitForText('Element1');
-        $I->click('Element1');
-        $I->waitForText('Formcycle');
-        $I->click('Formcycle');
-    }
-
-    #[Depends('createElementAndSave')]
-    public function seeFormSelection(
-        AcceptanceTester $I,
-        PageTree $pageTree,
-        ExtensionConfiguration $extensionConfiguration
-    ): void {
-        $extensionConfiguration->write('formcycleUrl', 'https://pro.form.cloud/formcycle');
-        $extensionConfiguration->write('formcycleClientId', '2252');
-        $this->navigateToElementTab($I, $pageTree);
-        $I->wait(1);
-        $I->dontSee('Configuration error');
-        $I->waitForElementVisible('#xm-available-forms-wrapper', 120);
     }
 
     public function createElementAndSave(
@@ -90,7 +49,12 @@ class ElementCest
 
         // Fill and save element
         $I->switchToContentFrame();
+        $I->wait(1);
         $I->fillField('input[data-formengine-input-name*="[header]"]', 'Element1');
+
+        $I->click('Formcycle');
+        $I->dontSee('Configuration error');
+        $I->waitForElementVisible('#xm-available-forms-wrapper', 120);
         $I->click('Save');
     }
 }
