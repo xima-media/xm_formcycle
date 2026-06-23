122effde2d14843b2686ed2212239211fdb6f8fc
Maik Schneider <maik.schneider@xima.de>
2026-06-23 13:42:52 +0200

    feat: install ddev add-on xima-media/ddev-playwright

---
+++ .ddev/playwright-build/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#ddev-generated
+# Remove the line above if you don't want this file to be overwritten when you run
+# ddev add-on get julienloizelet/ddev-playwright
+#
+# This file comes from https://github.com/julienloizelet/ddev-playwright
+#
+
+# Install DDEV certificate
+mkcert -install
+
+# Set up homeadditions if present
+if [ -d /mnt/ddev_config/.homeadditions ]; then
+    cp -r /mnt/ddev_config/.homeadditions/. $HOME/
+fi
+
+# Start KasmVNC server
+sudo -u "$(whoami)" vncserver -fg -disableBasicAuth
