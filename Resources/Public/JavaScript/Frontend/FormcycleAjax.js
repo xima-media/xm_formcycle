f34fff66d4d11b4cfd7a9e156852a5d7981deb9a
Maik Schneider <maik.schneider@xima.de>
2026-07-02 09:43:15 +0200

    feat: add loading and failure state for AJAX form loading
    Render a translated loading placeholder inside the AJAX container and pass a
    translated error message via a data attribute. The frontend script now shows
    that message on request failure instead of leaving the container empty.

--- Resources/Public/JavaScript/Frontend/FormcycleAjax.js
+++ Resources/Public/JavaScript/Frontend/FormcycleAjax.js
@@ -1,5 +1,6 @@
 for (const div of document.querySelectorAll('.xm-formcycle-ajax')) {
   const url = div.getAttribute('data-ajax-url')
+  const errorText = div.getAttribute('data-error-text') || ''
 
   $.ajax({
     url: url,
@@ -9,5 +10,7 @@ for (const div of document.querySelectorAll('.xm-formcycle-ajax')) {
     }
   }).done(data => {
     $(div).html(data)
+  }).fail(() => {
+    $(div).html(errorText)
   })
 }
